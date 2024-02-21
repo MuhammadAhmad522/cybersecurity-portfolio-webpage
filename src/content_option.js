98933c30883fe8a51dc858af98072a148fa837fb
MuhammadAhmad522 <muhammadahmad522@gmail.com>
2024-02-22 01:23:27 +0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,98 @@
+const logotext = "HappySecurity";
+const meta = {
+    title: "Ahmad",
+    description: "I'm Ahmad, a technologist and cybersecurity enthuist.",
+};
+
+const introdata = {
+    title: "Hi, I'm Ahmad",
+    animated: {
+        first: "Technologist",
+        second: "Visionary",
+        third: "Ethical Hacker",
+        fourth: "Programmer",
+        fifth: "Analyst",
+        sixth: "Developer",
+        seventh: "Security Researcher",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/yQGpm66/image.png",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I am a passionate computer enthusiast on the mission to explore the realms of science and technology. My journey began at the age of five, where I initially engaged with computers through gaming. This early exposure ignited my curiosity for the inner workings of these machines, fostering a desire for continuous exploration and learning. Today, I am committed to expanding my knowledge in the realm of computer science, with a primary focus on Cybersecurity. Moreover, I am actively involved in Web Development. Additionally, I hold a keen interest in Cloud Infrastructure and Quantum Computing, further reflecting my dedication to staying at the forefront of technological advancements.",
+};
+const worktimeline = [{
+    jobtitle: "Virtual Cybersecurity Intern",
+    where: "SAP",
+    date: "2023",
+},
+{
+    jobtitle: "Software Engineer Intern",
+    where: "SolutionsLoft",
+    date: "2023",
+}
+];
+
+const skills = [{
+    name: "Security Testing",
+},
+
+{
+    name: "Penetration Testing",
+},
+
+{
+    name: "MERN Stack",
+},
+
+{
+    name: "Kali Linux",
+},
+
+{
+    name: "Network Associate"
+},
+
+{
+    name: "Web-Exploitation",
+},
+{
+    name: "AWS"
+}
+];
+
+const dataportfolio = [{
+    img: "https://i.ibb.co/HC3nsRk/Untitledhackerrr.png",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://muhammadahmad522.github.io/Portfolio-Site/",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "muhammadahmad522@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "SERVICE_ID",
+    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+};
+
+const socialprofils = {
+    github: "https://github.com/MuhammadAhmad522",
+    linkedin: "https://www.linkedin.com/in/muhammad-ahmad-6521b01aa/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
